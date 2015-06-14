f6dd8382e65517b7150eef86860f8783e03baf21
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-14 16:17:40 -0400

    didn't work so reverted back to normal

--- config/env/all.js
+++ config/env/all.js
@@ -15,16 +15,17 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-                'public/lib/css/styles.css',
-                'public/lib/css/animate.css'
+                'http://fonts.googleapis.com/css?family=PT+Sans:400,700'
+                //'public/lib/css/styles.css',
+                //'public/lib/css/animate.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-                'http://code.jquery.com/jquery-1.11.3.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+                /*
                 'public/lib/js/backstretch.min.js',
                 'public/lib/js/counter.js',
                 'public/lib/js/jquery.waypoints.min.js',
@@ -35,7 +36,9 @@ module.exports = {
                 'public/lib/js/custom.js',
                 'public/lib/js/custom_hero.js',
                 'public/lib/js/custom_skills.js',
-                'public/lib/js/custom_countto.js'
+                'public/lib/js/custom_countto.js',
+                'public/lib/js/custom_google-map.js'
+                */
 			]
 		},
 		css: [
