a6e2d59df41e746f0196066b93acd573fe31f197
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-21 15:54:29 -0400

    styled map works

--- public/modules/reviews/controllers/reviews.client.controller.js
+++ public/modules/reviews/controllers/reviews.client.controller.js
@@ -151,17 +151,41 @@ angular.module('reviews').controller('ReviewsController', ['$scope', '$statePara
         });
         
         function initialize() {
+            //load grayscale style
+            var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+            
+            // Create a new StyledMapType object, passing it the array of styles,
+            // as well as the name to be displayed on the map type control.
+            var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+
+            
             var myLatlng = new google.maps.LatLng(43.6542904, -79.3854518);
             var mapOptions = {
-                zoom: 18,
-                center: myLatlng
-            }
+                zoom: 16,
+                center: myLatlng,
+                mapTypeControlOptions: false,
+                disableDefaultUI: true,
+                scaleControl: true,
+                zoomControl: true,
+                zoomControlOptions: {
+                  style: google.maps.ZoomControlStyle.LARGE 
+                }
+                /*
+                mapTypeControlOptions: {
+                    mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']  
+                }
+                */
+            };
+            
             var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
             var marker = new google.maps.Marker({
                 position: myLatlng,
                 map: map
             });
+            
+            map.mapTypes.set('map_style', styledMap);
+            map.setMapTypeId('map_style');
         }
 
         google.maps.event.addDomListener(window, 'load', initialize);
