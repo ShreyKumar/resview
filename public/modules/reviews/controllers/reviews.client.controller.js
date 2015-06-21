3b5494a0a46bce1f0a7ae95db0687bdf92513648
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-21 01:31:51 -0400

    added google maps api, easy as pi

--- public/modules/reviews/controllers/reviews.client.controller.js
+++ public/modules/reviews/controllers/reviews.client.controller.js
@@ -127,6 +127,7 @@ angular.module('reviews').controller('ReviewsController', ['$scope', '$statePara
             
             $("#view").addClass('active');
             $("#write").removeClass('active');
+            $("#writereview").hide();
             
             $("#write").unbind().click(function(){
                 //change navs
@@ -149,11 +150,40 @@ angular.module('reviews').controller('ReviewsController', ['$scope', '$statePara
             
         });
         
-        /*
-        $scope.write = function(){
-            console.log('hello');
+        function initialize() {
+            var myLatlng = new google.maps.LatLng(43.6542904, -79.3854518);
+            var mapOptions = {
+                zoom: 18,
+                center: myLatlng
+            }
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map
+            });
         }
-        */
+
+        google.maps.event.addDomListener(window, 'load', initialize);
         
+        
+            /*
+        function initialize() {
+            var res = new google.maps.LatLng(43.6542904, -79.3854518);
+            var mapOptions = {
+              center: res,
+              zoom: 18
+            };
+            
+            var marker = new google.maps.Marker({
+                position: res,
+                map: map,
+                title: 'Hello World!'
+            });
+            
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+        */
 	}
 ]);
